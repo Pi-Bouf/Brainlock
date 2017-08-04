df7dab27ccb5384ae916b6747b133db2ac1d9ca5
Pierre BOUFFIER <pierre.bouffier@e3d-environnement.com>
2017-08-04 11:51:57 +0200

    Config.php

--- include/config.php
+++ include/config.php
@@ -8,10 +8,10 @@ $salt = "0x874x0";
 $key = "zorro05?!";
 
 // MySQL
-$host = "mysql9.000webhost.com";
-$user = "a3264731_bl";
-$pass = "bouffier05";
-$base = "a3264731_bl";
+$host = "localhost";
+$user = "brainlock";
+$pass = "roroBrainlock.RCZ";
+$base = "brainlock";
 // MySQL connect
 $db = mysqli_connect($host, $user, $pass, $base);
 mysqli_set_charset($db, "UTF8");
